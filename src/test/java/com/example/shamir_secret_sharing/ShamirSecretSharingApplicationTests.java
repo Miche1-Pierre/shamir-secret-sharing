a7dd31778565e4cc7a3884aa7bfc7ba5e0a00886
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-01-27 19:22:57 +0100

    feat(config): Ajout des fichiers de configuration de base

---
+++ src/test/java/com/example/shamir_secret_sharing/ShamirSecretSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shamir_secret_sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShamirSecretSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
