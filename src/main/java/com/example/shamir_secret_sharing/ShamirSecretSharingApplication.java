ce30f182a8110eff4d7c88a5cabab02cdcb86684
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-01-27 19:24:15 +0100

    feat(Shamir App): Ajout du fichier de lancement de l'application

---
+++ src/main/java/com/example/shamir_secret_sharing/ShamirSecretSharingApplication.java
@@ -0,0 +1,11 @@
+package com.example.shamir_secret_sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShamirSecretSharingApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ShamirSecretSharingApplication.class, args);
+	}
+}
